d3752dd61b01a1855fc722097778976e4f9cc961
Anargyros Tsadimas <argiris@norbloc.com>
2024-12-15 18:49:09 +0200

    added swagger auth

--- src/main/java/gr/hua/dit/ds/ds_lab_2024/config/AppConfig.java
+++ src/main/java/gr/hua/dit/ds/ds_lab_2024/config/AppConfig.java
@@ -1,9 +1,16 @@
 package gr.hua.dit.ds.ds_lab_2024.config;
 
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @Configuration
 public class AppConfig {
@@ -14,11 +21,26 @@ public class AppConfig {
         return new BCryptPasswordEncoder();
     }
 
+
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+
     @Bean
-    public GroupedOpenApi controllerApi() {
-        return GroupedOpenApi.builder()
-                .group("controller-api")
-                .packagesToScan("gr.hua.dit.ds.ds_lab_2024") // Specify the package to scan
-                .build();
+    public OpenAPI openAPI() {
+        OpenAPI info = new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("DS LAB 2024 REST API")
+                        .description("This API is used in dl-lab-2024 project")
+                        .version("1.0").contact(new Contact().name("Anargyros Tsadimas")
+                                .email("tsadimas@hua.gr").url("https://tsadimas.github.io"))
+                        .license(new License().name("License of API")
+                                .url("https://swagger.io/license/")));
+        return info;
     }
 }
