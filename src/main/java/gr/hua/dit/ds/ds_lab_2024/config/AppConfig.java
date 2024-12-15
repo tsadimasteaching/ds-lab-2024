7364b295290f161945ed693fd2b2c7a97117b6be
Anargyros Tsadimas <argiris@norbloc.com>
2024-12-15 18:38:43 +0200

    added swagger

--- src/main/java/gr/hua/dit/ds/ds_lab_2024/config/AppConfig.java
+++ src/main/java/gr/hua/dit/ds/ds_lab_2024/config/AppConfig.java
@@ -1,5 +1,6 @@
 package gr.hua.dit.ds.ds_lab_2024.config;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,5 +12,13 @@ public class AppConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public GroupedOpenApi controllerApi() {
+        return GroupedOpenApi.builder()
+                .group("controller-api")
+                .packagesToScan("gr.hua.dit.ds.ds_lab_2024") // Specify the package to scan
+                .build();
     }
 }
