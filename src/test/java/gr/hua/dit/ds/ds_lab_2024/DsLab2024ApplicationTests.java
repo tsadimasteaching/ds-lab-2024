e3704e4b5c9fb68810b1abbd7842448d4f31e006
Anargyros Tsadimas <tsadimas@hua.gr>
2025-05-10 17:44:06 +0300

    added sample tests

--- src/test/java/gr/hua/dit/ds/ds_lab_2024/DsLab2024ApplicationTests.java
+++ src/test/java/gr/hua/dit/ds/ds_lab_2024/DsLab2024ApplicationTests.java
@@ -1,13 +1,55 @@
 package gr.hua.dit.ds.ds_lab_2024;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DsLab2024ApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void testCreateUser() throws Exception {
+		// Arrange
+		String userJson = "{\"username\":\"apiuser\",\"email\":\"api@hua.gr\",\"password\":\"pass123\"}";
+
+
+		// Act
+		ResultActions result = mockMvc.perform(post("/api/auth/signup")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(userJson));
+
+		// Assert
+		result.andExpect(status().isOk());
+	}
+
+	@Test
+	public void testSignUser() throws Exception {
+		// Arrange
+		String userJson = "{\"username\":\"apiuser\",\"password\":\"pass123\"}";
+
+		// Act
+		ResultActions result = mockMvc.perform(post("/api/auth/signin")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(userJson));
+
+		// Assert
+		result.andExpect(status().isOk())
+				.andExpect(jsonPath("$.username").value("apiuser"));
+
+	}
 }
